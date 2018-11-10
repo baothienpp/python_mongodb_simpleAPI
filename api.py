776e92febfa235f8f26eef9ceba7907937f6a039
Thien <baothien@gmail.com>
2018-11-10 19:40:44 +0100

    Implement Asynchronous I/O

--- api.py
+++ api.py
@@ -1,19 +1,31 @@
-from flask import Flask, request, jsonify
+from quart import Quart, request, Response
+import json
 from database import Database
 
-app = Flask(__name__)
+app = Quart(__name__)
 
 
 @app.route('/config', methods=['POST', 'GET'])
-def config():
+async def config():
     if request.method == 'POST':
-        db.push()
+        data = await request.get_json(force=True)
+        status = await db.push(data)
+        if status:
+            return Response(status=201, mimetype='application/json')
+        else:
+            return Response(status=400, mimetype='application/json')
+
     else:
         tenant = request.args.get('tenant')
         integration_type = request.args.get('integration_type')
-        result = db.query(tenant=tenant, integration_type=integration_type)
-        return result
+        result = await db.query(tenant=tenant, integration_type=integration_type)
+        if result:
+            response = Response(json.dumps(result), status=200, mimetype='application/json')
+            return response
+        else:
+            return Response('Document not found', status=404, mimetype='application/json')
 
-if __name__ == 'main':
+
+if __name__ == '__main__':
     db = Database()
-    app.run()
+    app.run(debug=True)
--- database.py
+++ database.py
@@ -1,22 +1,32 @@
 from pymongo import MongoClient
 
 
-class Database():
+class Database:
     def __init__(self):
         client = MongoClient('localhost', 27017)
         db = client['data']
         self.conf = db['configuration']
 
-    def push(self, data):
-        # User could post many docs, insert_one will block
-        # Maybe a buffer ? asynchro io ?
-        self.conf.insert_one(data)
+    async def push(self, data):
+        result = self.conf.insert_one(data)
 
-    def query(self, tenant=None, integration_type=None):
-        self.conf.find({'tenant': tenant, 'integration_type': integration_type})
+        if result.inserted_id:
+            return True
+        else:
+            return False
+
+    async def query(self, tenant=None, integration_type=None):
+        query = {'tenant': tenant, 'integration_type': integration_type}
+        query = {k: v for k, v in query.items() if v is not None}
+
+        result = []
+        for doc in self.conf.find(query):
+            doc.pop('_id')
+            result.append(doc)
+
+        return result
 
     def update(self):
         pass
 
 
-print('s')
